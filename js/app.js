49951a53023b134ae4775762e31e74e3f7a99467
Jeremy-nike <60948826+Jeremy-nike@users.noreply.github.com>
2020-02-11 15:55:13 -0800

    commit for parking lot

---
+++ js/app.js
@@ -0,0 +1,292 @@
+const contractSource = `
+payable contract ParkingLot =   
+    type i = int
+    type s = string
+    type a = address
+    type b = bool
+
+    record car = {
+        id : i,
+        owner : a,
+        nameOfCar	  : s,
+        nameOfOwner : s,
+        image : s,
+        lisencePlate	  : s,
+        entryDate : i,
+        exitDate : i,
+        checkedOut : b
+         }
+
+    record owner = {
+        owner : a,
+        nameOfOwner : s
+        }
+   
+    record state = 
+        { cars : map(i, car),
+        checkedInOwners : map(address, owner)}
+ 
+    entrypoint init() = {
+        cars = {},
+        checkedInOwners = {}}
+
+    entrypoint getCar(index : i) : car = 
+        switch(Map.lookup(index, state.cars))
+            None  => abort("There is no Cars with that ID.")
+            Some(x) => x  
+
+    stateful entrypoint addCar(nameOfCar' : s,nameOfOwner' : s, image' :s, lisencePlate' : s) = 
+
+        // Ensures the user has not already registered a car
+        require(!Map.member(Call.caller, state.checkedInOwners), "You are already checked in")
+        let index = getTotalCars() + 1
+        let car = {id= index,  
+            owner  = Call.caller,
+            nameOfCar = nameOfCar',
+            nameOfOwner = nameOfOwner',
+            image = image',
+            lisencePlate =  lisencePlate',
+            entryDate = Chain.timestamp,
+            exitDate = 0,
+            checkedOut = false   }
+
+        let checkedInOwner = {
+            owner = Call.caller,
+            nameOfOwner = nameOfOwner'
+
+            }
+        put(state {cars[index] = car})
+        put(state {checkedInOwners[Call.caller] = checkedInOwner})
+
+    private stateful function deleteUser() =
+          put(state{ checkedInOwners @ c = Map.delete(Call.caller, c) })
+
+    private stateful function updateCheckOutState(index : i) = 
+
+        let user  = getCar(index)
+
+        let updated = !user.checkedOut
+        put(state{cars[index].checkedOut = updated })
+
+
+    stateful payable entrypoint checkOut(index : i) = 
+        let carToCheckOut = getCar(index)
+        require(carToCheckOut.checkedOut != true, "This car has already been checkout out")
+        Chain.spend(Contract.address, 100000)
+
+        updateCheckOutState(index)
+        // Removes the user from the record
+        deleteUser()
+        
+        
+        let updateDate = state.cars[index].entryDate
+        
+        put(state{cars[index].exitDate = updateDate + Chain.block_height  })
+
+    entrypoint getTotalCars() : i = 
+        Map.size(state.cars)
+
+        
+
+        
+
+        
+        
+
+
+`;
+const contractAddress ='ct_HpyTW3d3QEcWPjX9REmfhzFcUH7DmroVLmee3d9gZSv9oCVEP';
+var client = null;
+var CarArray = [];
+
+
+
+async function callStatic(func, args) {
+  //Create a new contract instance that we can interact with
+  const contract = await client.getContractInstance(contractSource, {
+    contractAddress
+  });
+
+  const calledGet = await contract
+    .call(func, args, {
+      callStatic: true
+    })
+    .catch(e => console.error(e));
+
+  const decodedGet = await calledGet.decode().catch(e => console.error(e));
+  console.log("number of posts : ", decodedGet);
+  return decodedGet;
+}
+
+async function contractCall(func, args, value) {
+  const contract = await client.getContractInstance(contractSource, {
+    contractAddress
+  });
+  //Make a call to write smart contract func, with aeon value input
+  const calledSet = await contract
+    .call(func, args, {
+      amount: value
+    })
+    .catch(e => console.error(e));
+
+  return calledSet;
+}
+function renderCars()
+{
+    // CarArray = CarArray.sort(function(a,b){return b.Price - a.Price})
+    var template = $('#template').html();
+    
+    Mustache.parse(template);
+    var rendered = Mustache.render(template, {CarArray});
+
+    
+  
+
+    $('#cars').html(rendered);
+    console.log("for loop reached")
+}
+
+
+window.addEventListener('load', async () => {
+    $(".loading").show();
+  
+    client = await Ae.Aepp();
+  
+    totalCar =  await callStatic('getTotalCars', [])
+    console.log(totalCar)
+  
+    for (let i = 1; i <= totalCar; i++) {
+  
+     
+      const car = await callStatic('getCar', [i])
+      console.log(car)
+      console.log("This is the cars exit date : ", car.exitDate)
+  
+      CarArray.push({
+        id     : car.id,
+        owner           : car.owner,
+        nameOfCar          : car.nameOfCar,
+        image :car.image,
+        nameOfOwner          : car.nameOfOwner,
+        lisencePlate            : car.lisencePlate,
+        entryDate: Date(car.entryDate),
+        exitDate : 0,
+        checkedOut : car.checkedOut
+      })
+
+      renderCars();
+    }
+  
+    
+    
+    $(".loading").hide();
+  //   $("#loader").hide();
+  });
+  console.log("Finished")
+
+$('.modal-body').on('click', '#checkInBtn', async function () {
+  $(".loading").show();
+  console.log("Adding car to the blockchain")
+  // event.preventDefault();
+  console.log("Adding car to the blockchain")
+
+  
+  // const keys = ['Lisence_no', 'owner_name', 'nameOfCar', 'd_o_a', 'image', 'slot']
+
+  const Lisence_no = $('#Lisence_no').val()
+  
+  owner_name = ($('#owner_name').val());
+  nameOfCar = ($('#nameOfCar').val());
+
+  image = ($('#image').val())
+
+  
+
+
+ 
+  console.log(image)
+
+  const promise = await contractCall("addCar", [nameOfCar, owner_name, image, Lisence_no], 0)
+ if(promise !== undefined){
+
+   
+
+      // if(promise == undefined){
+      //   console.log("reverting request")
+      //   $(".loading").hide();
+
+      // }else{
+    const carId = await callStatic('getTotalCars', [])
+
+    const newCar = await callStatic('getCar', [carId])
+    CarArray.push({
+      id     : newCar.id,
+      owner           : newCar.owner,
+      nameOfCar          : newCar.nameOfCar,
+      image :newCar.image,
+      nameOfOwner          : newCar.nameOfOwner,
+      lisencePlate            : newCar.lisencePlate,
+      entryDate: Date(newCar.entryDate),
+      exitDate : 0,
+      checkedOut : newCar.checkedOut
+    })
+  
+    console.log("added")
+  
+    renderCars()
+  
+    $(".loading").hide();
+  
+
+}else{
+  console.log("Reverting request, Request failed")
+  $(".loading").hide();
+}
+})
+
+  
+  
+  $('#cars').on('click', '.checkOutBtn', async function (event) {
+    $(".loading").show();
+    console.log("Checking out")
+   
+    
+   
+    // const Lisence_no = $('#Lisence_no').val()
+    
+    // owner_name = ($('#owner_name').val());
+    // nameOfCar = ($('#nameOfCar').val());
+  
+    // image = ($('#image').val())
+  
+    // console.log(image)
+  
+    index = event.target.id
+    console.log("index", index)
+  
+    // const foundIndex = CarArray.findIndex(car => car.index == event.target.id);
+    // console.log("Found index", Math.abs(foundIndex))
+    // CarArray[Math.abs(foundIndex)].checkedOut = true;
+  
+  
+    console.log(index)
+    await contractCall("checkOut", [index], 100000)
+  
+    // const checkedOut = await callStatic('getCar', [index])
+  
+    // const checkOutDate = checkedOut.exitDate
+    // console.log("Check out date ",checkOutDate )
+  
+    // CarArray[Math.abs(foundIndex)].exitDate = Date();
+  
+    // console.log(checkedOut)
+  
+    location.reload()
+    console.log("checked out")
+  
+    renderCars()
+   
+ 
+
+  $(".loading").hide();
+})
